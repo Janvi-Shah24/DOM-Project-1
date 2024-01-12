ee76be67a7742aaf3f41734beae362ccbc67cfb6
Janvi <janvishah241198@gmail.com>
2024-01-12 19:25:30 +0530

    modified

---
+++ index.js
@@ -0,0 +1,14 @@
+let div = document.getElementById("box");
+let para = document.getElementById("box-txt");
+
+div.style.height = "200px";
+div.style.width = "300px";
+div.style.backgroundColor = "grey";
+div.style.margin = "22px";
+div.style.padding = "10px"
+
+para.style.fontSize = "20px";
+para.style.fontWeight = "bold";
+para.style.textAlign = "center";
+para.style.color = "aquamarine";
+para.style.paddingTop = "20px";
